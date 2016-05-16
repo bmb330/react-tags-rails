b085e92d86a49479bf3e2147a90fdb906ffe433d
Brandon Braun <brandon.braun@gmail.com>
2016-05-16 11:14:03 -0400

    generate gem

---
+++ lib/react/tags/rails.rb
@@ -0,0 +1,9 @@
+require "react/tags/rails/version"
+
+module React
+  module Tags
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/react/tags/rails_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe React::Tags::Rails do
+  it 'has a version number' do
+    expect(React::Tags::Rails::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
